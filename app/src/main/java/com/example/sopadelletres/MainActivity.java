48de33ce2612c8f287c76db33d79a77567b0f3be
Ivan <ivanraid@gmail.com>
2020-01-12 17:50:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sopadelletres/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sopadelletres;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.info_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //aqui falta poner para que vaya al webview
+        return super.onOptionsItemSelected(item);
+    }
+}
